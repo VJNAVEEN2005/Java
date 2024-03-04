1f9b62ea65c88ea75d822fad78b2709eb18aba1b
VJNAVEEN2005 <vjnaveen2005@gmail.com>
2024-03-04 17:36:24 +0530

    feat: Created counter

---
+++ Counter.java
@@ -0,0 +1,54 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class Counter{
+
+    TextField display;
+    Counter(){
+        Frame frame = new Frame("Counter");
+
+        display = new TextField("0");
+        display.setBounds(100,100,70,30);
+
+        Button btCount = new Button("Count");
+        btCount.setBounds(100,150,70,30);
+        Button btReset = new Button("Reset");
+        btReset.setBounds(100,200,70,30);
+
+        frame.add(btCount);
+        frame.add(btReset);
+        frame.add(display);
+
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setSize(500,500);
+
+        btCount.addActionListener(inc);
+        btReset.addActionListener(res);
+    }
+    int i=0;
+    ActionListener inc = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            i++;
+            display.setText(String.valueOf(i));
+            System.out.println("Counting: "+i);
+        }
+    };
+
+    ActionListener res = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            i=0;
+            display.setText(String.valueOf(i));
+            System.out.println("Reset: "+i);
+        }
+    };
+
+
+    public static void main(String[] args) {
+        int i=0;
+        Counter counter = new Counter();
+    }
+}
