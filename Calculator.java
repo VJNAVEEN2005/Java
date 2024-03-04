def91bd855e6d9dc08fe0fb3730626edb1f5ada5
VJNAVEEN2005 <vjnaveen2005@gmail.com>
2024-03-04 22:48:36 +0530

    !fix: Total Change (layout AWT , add sum only)

--- Calculator.java
+++ Calculator.java
@@ -1,43 +1,192 @@
-public class Calculator {
-    public static void main(String[] args) {
-        int a=10;
-        int b=12;
-        char c = '*';
-        float ans;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-        switch (c){
-            case '+':
-                ans=add(a,b);
-                System.out.println((int)ans);
-                break;
-            case '-':
-                ans=sub(a,b);
-                System.out.println((int)ans);
-                break;
-            case '*':
-                ans=mul(a,b);
-                System.out.println((int)ans);
-                break;
-            case '/':
-                ans=div(a,b);
-                System.out.println(ans);
-                break;
-            default:
-                System.out.println("Error");
+class Calculator{
+
+    Button bt1,bt2,bt3,bt4,bt5,bt6,bt7,bt8,bt9,bt0,btadd,btsub,btdiv,btmul,btequ,btdot;
+    TextField display;
+
+    Calculator(){
+        Frame frame =  new Frame("Calculator");
+
+        display = new TextField("");
+        display.setBounds(50,50,190,30);
+
+        bt0 = new Button("0");
+        bt0.setBounds(50,250,40,40);
+        bt1 = new Button("1");
+        bt1.setBounds(50,200,40,40);
+        bt2 = new Button("2");
+        bt2.setBounds(100,200,40,40);
+        bt3 = new Button("3");
+        bt3.setBounds(150,200,40,40);
+        bt4 = new Button("4");
+        bt4.setBounds(50,150,40,40);
+        bt5 = new Button("5");
+        bt5.setBounds(100,150,40,40);
+        bt6 = new Button("6");
+        bt6.setBounds(150,150,40,40);
+        bt7 = new Button("7");
+        bt7.setBounds(50,100,40,40);
+        bt8 = new Button("8");
+        bt8.setBounds(100,100,40,40);
+        bt9 = new Button("9");
+        bt9.setBounds(150,100,40,40);
+        btadd = new Button("+");
+        btadd.setBounds(200,200,40,40);
+        btsub = new Button("-");
+        btsub.setBounds(200,150,40,40);
+        btmul = new Button("*");
+        btmul.setBounds(200,100,40,40);
+        btdiv = new Button("/");
+        btdiv.setBounds(150,250,40,40);
+        btequ = new Button("=");
+        btequ.setBounds(200,250,40,40);
+        btdot =  new Button(".");
+        btdot.setBounds(100,250,40,40);
+
+        frame.add(display);
+        frame.add(bt7);
+        frame.add(bt8);
+        frame.add(bt9);
+        frame.add(btmul);
+        frame.add(bt4);
+        frame.add(bt5);
+        frame.add(bt6);
+        frame.add(btsub);
+        frame.add(bt1);
+        frame.add(bt2);
+        frame.add(bt3);
+        frame.add(btadd);
+        frame.add(bt0);
+        frame.add(btdot);
+        frame.add(btdiv);
+        frame.add(btequ);
+
+        bt1.addActionListener(_bt1);
+        bt2.addActionListener(_bt2);
+        bt3.addActionListener(_bt3);
+        bt4.addActionListener(_bt4);
+        bt5.addActionListener(_bt5);
+        bt6.addActionListener(_bt6);
+        bt7.addActionListener(_bt7);
+        bt8.addActionListener(_bt8);
+        bt9.addActionListener(_bt9);
+        bt0.addActionListener(_bt0);
+        btdot.addActionListener(_btdot);
+        btadd.addActionListener(_btadd);
+        btequ.addActionListener(_btequ);
+
+        frame.setLayout(null);
+        frame.setSize(500,500);
+        frame.setVisible(true);
+    }
+
+    ActionListener _bt1 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            display.setText(display.getText()+"1");
         }
+    };
 
-    }
+    ActionListener _bt2 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            display.setText(display.getText()+"2");
+        }
+    };
 
-    public static int add(int x, int y){
-        return x+y;
-    }
-    public static int sub(int x, int y){
-        return x-y;
-    }
-    public static int mul(int x, int y){
-        return x*y;
-    }
-    public static float div(int x, int y){
-        return (float)x/(float)y;
+    ActionListener _bt3 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            display.setText(display.getText()+"3");
+        }
+    };
+
+    ActionListener _bt4 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            display.setText(display.getText()+"4");
+        }
+    };
+
+    ActionListener _bt5 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            display.setText(display.getText()+"5");
+        }
+    };
+
+    ActionListener _bt6 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            display.setText(display.getText()+"6");
+        }
+    };
+
+    ActionListener _bt7 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            display.setText(display.getText()+"7");
+        }
+    };
+
+    ActionListener _bt8 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            display.setText(display.getText()+"8");
+        }
+    };
+
+    ActionListener _bt9 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            display.setText(display.getText()+"9");
+        }
+    };
+
+    ActionListener _bt0 = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            display.setText(display.getText()+"0");
+        }
+    };
+
+    ActionListener _btdot = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            display.setText(display.getText()+".");
+        }
+    };
+
+    //the operation start from here
+    float num1,num2;
+    ActionListener _btadd = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            num1 = Float.parseFloat(display.getText());
+            display.setText("");
+        }
+    };
+
+    ActionListener _btsub = new ActionListener() {                  //not finished
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            num1 = Float.parseFloat(display.getText());
+            display.setText("");
+        }
+    };
+
+    ActionListener _btequ = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            num2 = Float.parseFloat(display.getText());
+            display.setText(String.valueOf(num1+num2));
+        }
+    };
+
+    public static void main(String[] args) {
+        new Calculator();
     }
 }
